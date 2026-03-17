0c10ed325ed39f613a2fe6c0caf0980f2c8d6d0b
toums404 <tomthunus2@gmail.com>
2026-03-17 09:48:30 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace POO_PortSerie
+{
+    partial class FenetrePrincipale
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.lblCom = new System.Windows.Forms.Label();
+            this.bOuvrirFermer = new System.Windows.Forms.Button();
+            this.tbSortie = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bSendGps = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbPortEcriture = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Items.AddRange(new object[] {
+            "COM1",
+            "COM2"});
+            this.cmbPorts.Location = new System.Drawing.Point(307, 37);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(103, 28);
+            this.cmbPorts.TabIndex = 0;
+            this.cmbPorts.SelectedIndexChanged += new System.EventHandler(this.cmbPorts_SelectedIndexChanged);
+            // 
+            // lblCom
+            // 
+            this.lblCom.AutoSize = true;
+            this.lblCom.Location = new System.Drawing.Point(8, 40);
+            this.lblCom.Name = "lblCom";
+            this.lblCom.Size = new System.Drawing.Size(293, 20);
+            this.lblCom.TabIndex = 1;
+            this.lblCom.Text = "Veuillez choisir le port COM de réception";
+            // 
+            // bOuvrirFermer
+            // 
+            this.bOuvrirFermer.Location = new System.Drawing.Point(12, 107);
+            this.bOuvrirFermer.Name = "bOuvrirFermer";
+            this.bOuvrirFermer.Size = new System.Drawing.Size(103, 93);
+            this.bOuvrirFermer.TabIndex = 2;
+            this.bOuvrirFermer.Text = "Appuyez pour ouvrir ou fermer la connexion";
+            this.bOuvrirFermer.UseVisualStyleBackColor = true;
+            this.bOuvrirFermer.Click += new System.EventHandler(this.bOuvrirFermer_Click);
+            // 
+            // tbSortie
+            // 
+            this.tbSortie.Location = new System.Drawing.Point(198, 249);
+            this.tbSortie.Multiline = true;
+            this.tbSortie.Name = "tbSortie";
+            this.tbSortie.Size = new System.Drawing.Size(331, 156);
+            this.tbSortie.TabIndex = 3;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 84);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(121, 20);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Statut : inconnu";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // bSendGps
+            // 
+            this.bSendGps.Location = new System.Drawing.Point(12, 249);
+            this.bSendGps.Name = "bSendGps";
+            this.bSendGps.Size = new System.Drawing.Size(164, 93);
+            this.bSendGps.TabIndex = 7;
+            this.bSendGps.Text = "Démarrer/arreter l\'envoi des trames gps";
+            this.bSendGps.UseVisualStyleBackColor = true;
+            this.bSendGps.Click += new System.EventHandler(this.bSendGps_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(254, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Veuillez choisir le port COM d\'envoi";
+            // 
+            // cmbPortEcriture
+            // 
+            this.cmbPortEcriture.FormattingEnabled = true;
+            this.cmbPortEcriture.Items.AddRange(new object[] {
+            "COM1",
+            "COM2"});
+            this.cmbPortEcriture.Location = new System.Drawing.Point(272, 212);
+            this.cmbPortEcriture.Name = "cmbPortEcriture";
+            this.cmbPortEcriture.Size = new System.Drawing.Size(103, 28);
+            this.cmbPortEcriture.TabIndex = 5;
+            // 
+            // FenetrePrincipale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bSendGps);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbPortEcriture);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.tbSortie);
+            this.Controls.Add(this.bOuvrirFermer);
+            this.Controls.Add(this.lblCom);
+            this.Controls.Add(this.cmbPorts);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FenetrePrincipale";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Port Série";
+            this.Load += new System.EventHandler(this.FenetrePrincipale_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Label lblCom;
+        private System.Windows.Forms.Button bOuvrirFermer;
+        private System.Windows.Forms.TextBox tbSortie;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button bSendGps;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbPortEcriture;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POO_PortSerie
+{
+    public partial class FenetrePrincipale : Form
+    {
+        private SerialPort portALire;
+        private SerialPort portAEcrire;
+        public FenetrePrincipale()
+        {
+            InitializeComponent();
+        }
+
+        private void FenetrePrincipale_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cmbPorts.Items.AddRange(ports);
+            cmbPortEcriture.Items.AddRange(ports);
+            if (ports.Length > 1)
+            {
+                cmbPortEcriture.SelectedIndex = 1;
+            }
+            else if (ports.Length > 0)
+            {
+                cmbPortEcriture.SelectedIndex = 0;
+            }
+            if (ports.Length > 0)
+            {
+                cmbPorts.SelectedIndex = 0;
+            }
+            else
+            {
+                MessageBox.Show("pas de port serie detecté sur cet ordi");
+                bOuvrirFermer.Enabled = false;
+            }
+        }
+
+        private void bOuvrirFermer_Click(object sender, EventArgs e)
+        {
+            if (portALire != null && portALire.IsOpen)
+            {
+                portALire.Close();
+                bOuvrirFermer.Text = "ouvrir le port";
+                cmbPorts.Enabled = true;
+            }
+            else
+            {
+                if (cmbPorts.SelectedItem != null)
+                {
+                    string selectedPort = cmbPorts.SelectedItem.ToString();
+                    //initialisation des ports
+                    portALire = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+                    portALire.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                    try
+                    {
+                        portALire.Open();
+                        bOuvrirFermer.Text = "fermer le port";
+                        cmbPorts.Enabled = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"erreur lors de l'ouverture du port : {ex.Message}", "erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = portALire.ReadLine();
+
+                this.Invoke((MethodInvoker)delegate //invoke pour mettre à jour l'interface graphique depuis un thread secondaire
+                {
+                    tbSortie.AppendText($"Donnée reçue sur {portALire.PortName}: {data}" + Environment.NewLine);
+
+                    tbSortie.SelectionStart = tbSortie.Text.Length;
+                    tbSortie.ScrollToCaret();
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (portALire != null && portALire.IsOpen)
+            {
+                portALire.Close();   
+            }
+            portAEcrire?.Close();
+        }
+
+        private void cmbPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (portALire != null && portALire.IsOpen)
+                return;
+
+            string selectedPort = cmbPorts.SelectedItem.ToString();
+            try
+            {
+                using (SerialPort testPort = new SerialPort(selectedPort))//creation de port temp pour tester l'etat
+                {
+                    testPort.Open();
+                    testPort.Close(); 
+
+                    lblStatus.Text = "Statut : Disponible";
+                    lblStatus.ForeColor = Color.Green;
+                    bOuvrirFermer.Enabled = true;
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // acces refusé donc port deja ouvert par autre chose
+                lblStatus.Text = "Statut :Occupé ";
+                lblStatus.ForeColor = Color.Red;
+                bOuvrirFermer.Enabled = false;
+            }
+            catch (Exception)
+            {
+                //autre erreur
+                lblStatus.Text = "Statut : inconnu/inutilisable";
+                lblStatus.ForeColor = Color.Orange;
+                bOuvrirFermer.Enabled = false;
+            }
+        }
+
+        private void bSendGps_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+
+                if (portAEcrire != null && portAEcrire.IsOpen)
+                {
+                    portAEcrire.Close();
+                }
+
+                bSendGps.Text = "demarrer l'envoi GPS";
+                cmbPortEcriture.Enabled = true;
+                tbSortie.AppendText("------ Envoi GPS arrêté ------" + Environment.NewLine);
+            }
+            else
+            {
+
+                if (cmbPortEcriture.SelectedItem != null)
+                {
+                    string selectedWritePort = cmbPortEcriture.SelectedItem.ToString();
+
+                    // verif si port d'ecriture different du port de lecture
+                    if (cmbPorts.SelectedItem != null && selectedWritePort == cmbPorts.SelectedItem.ToString())
+                    {
+                        MessageBox.Show("veuillez choisir un port d'écriture différent du port de lecture", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+
+                    try
+                    {
+                        portAEcrire = new SerialPort(selectedWritePort, 9600, Parity.None, 8, StopBits.One);
+                        portAEcrire.Open();
+
+                        timer1.Start();
+
+                        bSendGps.Text = "arrêter l'envoi GPS";
+                        cmbPortEcriture.Enabled = false;
+                        tbSortie.AppendText($"==> début de l'envoi sur {selectedWritePort}..." + Environment.NewLine);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"erreur lors de l'ouverture du port d'écriture : {ex.Message}", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (portAEcrire != null && portAEcrire.IsOpen)
+            {
+                try
+                {
+                    DateTime utcNow = DateTime.UtcNow;
+                    string formattedTime = utcNow.ToString("HHmmss");
+
+                    //Trame prof
+                    string nmeaMessage = "$GPGGA," + formattedTime + ",5037.2947,N,00535.2579,E,1,10,0.8,100.0,M,45.0,M,,*6A";
+
+                    portAEcrire.WriteLine(nmeaMessage);
+
+
+                   tbSortie.AppendText($"Donnée envoyée par {portAEcrire.PortName}" + Environment.NewLine);
+                    tbSortie.SelectionStart = tbSortie.Text.Length;
+                    tbSortie.ScrollToCaret();
+                }
+                catch (Exception ex)
+                {
+                    timer1.Stop();
+                    MessageBox.Show($"Erreur d'écriture : {ex.Message}");
+                }
+            }
+        }
+    }
+
+}
+
